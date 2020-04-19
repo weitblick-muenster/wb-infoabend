48a45b17f480d1ae5965ce2698beb847090f8ed8
Manh Tin <mtnguyen47@gmail.com>
2020-04-19 22:06:58 +0200

    change footer

--- components/Page/TheFooter.vue
+++ components/Page/TheFooter.vue
@@ -1,44 +1,23 @@
 <template>
   <footer id="footer">
-    <div class="inner">
-      <section>
-        <h2>Folge uns!</h2>
-        <ul class="vertical">
-          <li>
-            <a
-              href="https://www.facebook.com/WeitblickMuenster"
-              class="icon brands style2 fa-facebook-f"
-            >
-              <span class="label">Facebook</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/weitblick_muenster/"
-              class="icon brands style2 fa-instagram"
-            >
-              <span class="label">Instagram</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/ManhTin/wb-infoabend"
-              class="icon brands style2 fa-github"
-            >
-              <span class="label">GitHub</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="mailto:muenster@weitblicker.org"
-              class="icon solid style2 fa-envelope"
-            >
-              <span class="label">Email</span>
-            </a>
-          </li>
-        </ul>
-      </section>
-    </div>
+    <ul class="vertical">
+      <li>
+        <a
+          href="#"
+          class="is-underlined"
+        >
+          Impressum
+        </a>
+      </li>
+      <li>
+        <a
+          href="#"
+          class="is-underlined"
+        >
+          Datenschutz
+        </a>
+      </li>
+    </ul>
   </footer>
 </template>
 
