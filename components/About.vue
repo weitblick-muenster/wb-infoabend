29e9d0bbc7fb7a3f7caaee032b613b654e98bc0d
Manh Tin <mtnguyen47@gmail.com>
2020-04-19 22:05:26 +0200

    move intro from about

--- components/About.vue
+++ components/About.vue
@@ -1,51 +1,5 @@
 <template>
-  <div>
-    <h1>Hey, willkommen zur Infoveranstaltung!</h1>
-    <p>
-      Schön dass Du zu uns gefunden hast! Da Veranstaltungen aufgrund des aktuellen Kontaktverbotes
-      nur eingeschränkt stattfinden können, halten wir die diesjährige Infoveranstaltung online ab.
-      Du fragst Dich wie? Auf dieser Seite findest Du vorab eine Informationssammlung zu Weitblick
-      und allen Gruppen in Form von kurzen Texten und Videos. So kannst Du Dich schonmal vertraut
-      machen mit unseren Gruppen.<br>
-      <span class="underlined">Am 7. Mai 2020 um 20 Uhr</span> findet dann die eigentliche
-      Infoveranstaltung als Online-Videokonferenz statt. Da hast Du dann die Möglichkeit die
-      Gruppen kennenzulernen und direkt Deine Fragen zu stellen. Unten findest du ein Formular,
-      über welches Du den Gruppen deine Kontaktdaten hinterlassen kannst. Diese melden sich dann
-      bei Dir.
-      Teilnehmen kannst du direkt über den Webbrowser ohne Installation eines Programmes.
-      Wir empfehlen Dir aber dies mit deinem Computer zu machen.
-    </p>
-    <table>
-      <tbody>
-        <tr>
-          <td>Wann</td>
-          <td><a class="underlined">Donnerstag, 7. Mai 2020 ab 20 Uhr</a></td>
-        </tr>
-        <tr>
-          <td>Wo</td>
-          <td><a class="underlined">Bei Dir zu Hause</a></td>
-        </tr>
-      </tbody>
-    </table>
-    <ul class="vertical">
-      <li>
-        <a
-          href="#"
-          class="button primary"
-        >
-          <span class="label">Teilnehmen</span>
-        </a>
-      </li>
-      <li>
-        <NuxtLink
-          to="/form"
-          class="button primary"
-        >
-          <span class="label">Formular</span>
-        </NuxtLink>
-      </li>
-    </ul>
-
+  <section>
     <h1>Über Weitblick</h1>
     <p>
       Weitblick ist eine Studierendeninitiative an der Uni Münster.
@@ -63,7 +17,7 @@
       aktiv zu werden oder uns auch
       passiv zu unterstützen.<br>
     </p>
-  </div>
+  </section>
 </template>
 
 <script>
