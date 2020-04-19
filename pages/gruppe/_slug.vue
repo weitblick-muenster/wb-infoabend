c4367e939ab3fb6bfa12dcdd412d54c7ec866bad
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-04-19 21:04:12 +0200

    add details to group page

--- pages/gruppe/_slug.vue
+++ pages/gruppe/_slug.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
-    {{ group.name }}
+    <h1>{{ group.name }}</h1>
+
+    <span class="image main">
+      <img :src="imageSrc">
+    </span>
+
+    <p>{{ group.description }}</p>
+
+    <iframe
+      :src="group.videoSrc"
+      width="560"
+      height="315"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
   </div>
 </template>
 
 <script>
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 import groups from '~/data/groups';
 
 export default {
@@ -17,6 +34,11 @@ export default {
     }
 
     return error({ statusCode: 404, message: 'Gruppe nicht gefunden' });
+  },
+  computed: {
+    imageSrc() {
+      return require(`~/assets/images/${this.group.imgSrc}`);
+    },
   },
 };
 </script>
