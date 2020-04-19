caed420953739d818b833a138fe28475716531c8
Manh Tin <mtnguyen47@gmail.com>
2020-04-19 22:04:54 +0200

    add follow component

---
+++ components/Follow.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h1>Folge uns!</h1>
+    <p>
+      Du willst auf dem Laufenden bleiben? Folge uns doch auf unseren Social Media Kanälen
+      und verpasse keine Neuigkeiten!
+    </p>
+    <ul class="vertical">
+      <li>
+        <a
+          href="https://www.instagram.com/weitblick_muenster/"
+          class="icon brands style2 fa-instagram"
+        >
+          <span class="label">Instagram</span>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.facebook.com/WeitblickMuenster"
+          class="icon brands style2 fa-facebook-f"
+        >
+          <span class="label">Facebook</span>
+        </a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
