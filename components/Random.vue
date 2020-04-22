7ae2eb8c19c405fa1b804de00192b9c92042c52c
Manh Tin <mtnguyen47@gmail.com>
2020-04-22 23:36:07 +0200

    add confetti magic

--- components/Random.vue
+++ components/Random.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
+    <h1>Glücksrad</h1>
     <p>
-      Puh, ganz schön Auswahl! Gar keine Ahnung welche Gruppe Du Dir zuerst ansehen möchtest?
+      Puh, ganz schön Auswahl! Gar keine Ahnung welche Gruppe Du Dir ansehen möchtest?
       Wir haben uns was für Dich überlegt. Klicke unten auf den Button und wir bringen Dich
       zu einer zufälligen Gruppe.
     </p>
@@ -16,13 +17,19 @@
 </template>
 
 <script>
+import { confetti } from 'dom-confetti';
 import groups from '~/data/groups';
 
 export default {
   methods: {
     randomGroup() {
       const randGroup = groups[Math.floor(Math.random() * groups.length)];
-      window.location.href = `${window.location.origin}/gruppe/${randGroup.slug}`;
+      const button = document.querySelector('.random');
+
+      confetti(button);
+      setTimeout(() => {
+        window.location.href = `/gruppe/${randGroup.slug}`;
+      }, 1500);
     },
   },
 };
