c08c40178407757b94a7f5bc00c55b71a9ed5c72
Manh Tin <mtnguyen47@gmail.com>
2020-04-22 22:10:28 +0200

    add basic random button behaviour

--- components/Random.vue
+++ components/Random.vue
@@ -3,18 +3,27 @@
     <p>
       Puh, ganz schön Auswahl! Gar keine Ahnung welche Gruppe Du Dir zuerst ansehen möchtest?
       Wir haben uns was für Dich überlegt. Klicke unten auf den Button und wir bringen Dich
-      zu einer zufälligen Gruppe inklusive Konfetti!
+      zu einer zufälligen Gruppe.
     </p>
-    <NuxtLink
+    <a
       tag="button"
-      to="/g"
-      class="button primary"
+      class="random button primary"
+      @click="randomGroup"
     >
-      <span class="label">mitmachen</span>
-    </NuxtLink>
+      Auf gut Glück
+    </a>
   </section>
 </template>
 
 <script>
-export default {};
+import groups from '~/data/groups';
+
+export default {
+  methods: {
+    randomGroup() {
+      const randGroup = groups[Math.floor(Math.random() * groups.length)];
+      window.location.href = `${window.location.origin}/gruppe/${randGroup.slug}`;
+    },
+  },
+};
 </script>
