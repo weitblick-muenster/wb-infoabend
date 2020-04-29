7b1d339832859faf4b1e0ad0d8c53af35fc8ff64
Manh Tin <mtnguyen47@gmail.com>
2020-04-29 14:23:23 +0200

    add noreferrer and tweak texts

--- components/Join.vue
+++ components/Join.vue
@@ -6,20 +6,22 @@
       werden? Dann kannst du hier die Beitrittserklärung herunterladen. Diese
       kannst du bequem am Laptop ausfüllen und unterschrieben an
       <a
-        href="mailto:buero-muenster@weitblicker.org"
+        href="mailto:buero-muenster@weitblicker.org?subject=Beitrittserklärung"
         class="is-underlined"
       >
         buero-muenster@weitblicker.org
       </a>
-      schicken. Wenn wir deine Anmeldung bearbeitet haben, bekommst du eine Bestätigung per Mail.
+      schicken. Wenn wir deine Anmeldung bearbeitet haben, bekommst du eine Bestätigung per E-Mail.
       Da in Zeiten von Social Distancing unser Vereinsleben rein digital abläuft, raten wir dazu,
       das Einverständnis zum Erstellen eines Intranet-Accounts auf der Beitrittserklärung
-      zu erteilen.
+      zu erteilen, Damit du Zugriff auf entsprechende Tools, sowie deine persönliche Weitblick
+      E-Mail Adresse bekommst.
     </p>
     <a
       href="https://weitblicker.org/media/documents/muenster/beitrittserklaerung.pdf"
       class="button primary"
       target="_blank"
+      rel="noreferrer"
     >
       <span class="label">Beitrittserklärung</span>
     </a>
