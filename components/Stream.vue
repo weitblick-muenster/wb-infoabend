85100b9ac807e7bf3e4ef54bdcd631bc03233494
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-05-06 20:51:21 +0200

    improve stream page

--- components/Stream.vue
+++ components/Stream.vue
@@ -1,35 +1,63 @@
 <template>
-  <section style="max-width: 1400px;">
-    <div class="stream">
-      <div class="lazyload stream-left">
-        <iframe
-          title="Weitblick Live Stream"
-          src="https://www.youtube-nocookie.com/embed/kr91LP4ecDk"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope;"
-          allowfullscreen
-        />
-      </div>
-      <div class="lazyload stream-right">
-        <iframe
-          title="Weitblick Live Stream"
-          :src="liveChatSrc"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope;"
-          allowfullscreen
-        />
+  <div class="section has-background-grey">
+    <div class="container stream-container">
+      <div class="stream">
+        <div class="lazyload stream-left">
+          <iframe
+            title="Weitblick Live Stream"
+            src="https://www.youtube-nocookie.com/embed/kr91LP4ecDk"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope;"
+            allowfullscreen
+          />
+        </div>
+        <div
+          v-if="showChat"
+          class="lazyload stream-right"
+        >
+          <iframe
+            title="Weitblick Live Stream"
+            :src="liveChatSrc"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope;"
+            allowfullscreen
+          />
+        </div>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
+import { isMobile, isTablet } from '~/lib/userAgent';
+
 export default {
+  data() {
+    return {
+      showChat: false,
+    };
+  },
   computed: {
     liveChatSrc() {
       const host = process.env.NODE_ENV === 'development' ? 'localhost' : 'infoabend.weitblicker.live';
       return `https://www.youtube.com/live_chat?v=kr91LP4ecDk&embed_domain=${host}`;
     },
   },
+  mounted() {
+    if (!isMobile(navigator.userAgent) && !isTablet(navigator.userAgent)) {
+      this.showChat = true;
+    }
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.has-background-grey {
+  background-color: lightgrey;
+}
+
+.stream-container {
+  max-width: 1400px;
+  margin-bottom: 20px;
+}
+</style>
--- components/StreamInfo.vue
+++ components/StreamInfo.vue
@@ -1,36 +1,59 @@
-<template>
-  <section>
-    <h1>Live Stream</h1>
-    <p>
-      Ab 20 Uhr gibt es eine ca. 45 minütige Präsentation über Weitblick und unsere
-      Arbeit. Wenn du während der Präsentation Dinge nicht verstehst oder Fragen hast,
-      stelle diese gerne im Chat! Unsere Moderator*innen werden sie nebenbei beantworten.
-      Wenn du Hilfe benötigst oder etwas nicht klappt, findest du
-      <a
-        href="#"
-        class="is-underlined"
-      >
-        hier
-      </a>
-      Hilfe.
-    </p>
-    <h2>Speeddating</h2>
-    <p>
-      Nach der Präsentation wird es ein Speeddating geben. Was bedeutet das? Da wir in vielen
-      Gruppen arbeiten, hast du die Möglichkeit - wie beim Speeddating - ca. 15 Minuten lang eine
-      Gruppe kennenzulernen, danach geht es zur nächsten. Klicke dazu einfach auf eine der unten
-      aufgeführten Gruppen, darüber gelangst du zum Video-Call. Wir werden im Stream bescheid
-      geben, ab wann das Speeddating startet.
-    </p>
-    <h2>Altstadt danach</h2>
-    <p>
-      Altstadt danach? Leider nein...Aber wir holen die Altstadt zu dir nach Hause! Unser Kneipen
-      Feature ermöglicht es, einfach in lockerer Runde ein Getränk deiner Wahl zu genießen und
-      dabei Weitblicker*innen und neue kennen zu lernen!
-    </p>
-  </section>
+<template functional>
+  <div>
+    <div class="section">
+      <div class="container">
+        <h1>Live Stream</h1>
+        <p>
+          Ab 20 Uhr gibt es eine ca. 45 minütige Präsentation über Weitblick und unsere
+          Arbeit. Wenn du während der Präsentation Dinge nicht verstehst oder Fragen hast,
+          stelle diese gerne im Chat! Unsere Moderator*innen werden sie nebenbei beantworten.
+          Wenn du Hilfe benötigst oder etwas nicht klappt, findest du
+          <a
+            href="#"
+            class="is-underlined"
+          >
+            hier
+          </a>
+          Hilfe.
+        </p>
+      </div>
+    </div>
+
+    <div class="section has-background-grey">
+      <div class="container">
+        <h2>Speeddating</h2>
+        <p>
+          Nach der Präsentation wird es ein Speeddating geben. Was bedeutet das? Da wir in vielen
+          Gruppen arbeiten, hast du die Möglichkeit - wie beim Speeddating - ca. 15 Minuten lang
+          eine Gruppe kennenzulernen, danach geht es zur nächsten. Klicke dazu einfach auf eine der
+          unten aufgeführten Gruppen, darüber gelangst du zum Video-Call. Wir werden im Stream
+          Bescheid geben, ab wann das Speeddating startet.
+        </p>
+        <h2>Altstadt danach</h2>
+        <p>
+          Altstadt danach? Leider nein...Aber wir holen die Altstadt zu dir nach Hause! Unser
+          Kneipen-Feature ermöglicht es, einfach in lockerer Runde ein Getränk deiner Wahl zu
+          genießen und dabei Weitblicker*innen und neue kennen zu lernen!
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
-</script>
+<style lang="scss" scoped>
+h1 {
+  font-size: 20px;
+}
+
+h2 {
+  font-size: 16px;
+}
+
+p {
+  font-size: 15px
+}
+
+.has-background-grey {
+  background-color: lightgrey;
+}
+</style>
