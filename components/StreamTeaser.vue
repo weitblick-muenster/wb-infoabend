404e65f950fd5be8f9e443c3113268ba5c32c6f4
Manh Tin <mtnguyen47@gmail.com>
2020-05-07 03:11:31 +0200

    add stream teaser Co-authored-by: pantajosef <pantazidisjosef@gmail.com>

--- components/StreamTeaser.vue
+++ components/StreamTeaser.vue
@@ -1,17 +1,62 @@
 <template>
   <section>
-    <NuxtLink
-      to="/stream"
+    <div
+      v-if="showStream"
+      class="countdown"
     >
-      <img
-        src="~/assets/images/wb-stream-cta.gif"
-        class="stream-teaser-image"
-        alt="Collage von Weitblickern"
-      >
-    </NuxtLink>
+      <NuxtLink to="/stream">
+        <img
+          src="~/assets/images/wb-stream-cta.gif"
+          class="stream-thumbnail"
+        >
+      </NuxtLink>
+    </div>
+
+    <template v-else>
+      <div class="countdown">
+        <ClientOnly>
+          <Countdown
+            :deadline="streamStartingAt"
+            @deadlineEnd="onCountdownFinish"
+          />
+        </ClientOnly>
+        <div
+          ref="countdownConfetti"
+          class="countdown-confetti"
+        />
+        <p class="stream-info-text">
+          Der Live-Stream hat noch nicht gestartet schau<br>
+          dir in der Zwischenzeit doch unsere Gruppen an.<br>
+          ⬇️
+        </p>
+      </div>
+    </template>
   </section>
 </template>
 
 <script>
-export default {};
+import { confetti } from 'dom-confetti';
+import Countdown from './Countdown.vue';
+
+export default {
+  components: {
+    Countdown,
+  },
+  data() {
+    return {
+      streamStartingAt: '2020-05-07T19:50:00',
+      showStream: false,
+    };
+  },
+  created() {
+    this.showStream = new Date() > new Date(this.streamStartingAt);
+  },
+  methods: {
+    onCountdownFinish() {
+      confetti(this.$refs.countdownConfetti);
+      const vm = this;
+      setTimeout(() => { vm.showStream = true; }, 3000);
+    },
+  },
+};
 </script>
