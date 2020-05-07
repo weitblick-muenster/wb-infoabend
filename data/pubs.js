110535e51c6fcb45ac3c66196a073c316834809a
Tobias Görtz <tobias.goertz@zweitag.de>
2020-05-07 13:24:30 +0200

    add new teams links to pubs

--- data/pubs.js
+++ data/pubs.js
@@ -2,46 +2,46 @@ export default [
   {
     name: 'Dille',
     imgSrc: 'pubs/dille.jpeg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_MjU1NzkwZTEtN2MzYy00Yzc1LTgxMTgtYmIyNDE2OGIyMzIz%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
   {
     name: 'Barzillus',
     imgSrc: 'pubs/barzillus.jpg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_OThkMWYzNTktNTkyZC00YTdiLTkxY2YtZGNkY2VjNjViMTU2%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
   {
     name: 'Himmel & Hölle',
     imgSrc: 'pubs/himmel-hoelle.jpeg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZWJjZDc0Y2MtYjE3ZC00ZDQzLWJkM2YtODI0Y2NkYzgzODY4%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
   {
     name: 'Cavete',
     imgSrc: 'pubs/cavete.jpg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_YmM4NjAwYmItNjYxZS00MDE0LTk3ZWMtOTM2MmJkYTUyNjIz%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
   {
     name: 'Fyal',
     imgSrc: 'pubs/fyal.jpg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_YjIwMTQ4MzgtZmJkZS00ZjdmLWE5YmEtMjQwOGI5ZjAyMzc2%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
   {
     name: 'Rote Liebe',
     imgSrc: 'pubs/roteliebe.jpg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ODExYTJhZjktOTgwZi00MzJjLTg4ZmUtYzdhODllYjQ0YmMy%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
   {
     name: 'Blaues Haus',
     imgSrc: 'pubs/balues-haus.jpg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZTEwNTc3YWQtNGVjMy00OGEzLWEwNzctYzNjZjE0Y2ViNTlj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
   {
     name: 'Teilchen & Beschleuniger',
     imgSrc: 'pubs/teilchen.jpg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_MzhjYThkMjAtMTk0MS00MDIyLWJlZTgtMzZlZmFkMjY4ODYw%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
   {
     name: 'Babel',
     imgSrc: 'pubs/babel.jpeg',
-    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmVlMjFlMTQtZjBmYy00M2QwLThjNGMtNGYxZDhhZDIxNmRj%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
+    teams: 'https://teams.microsoft.com/l/meetup-join/19%3ameeting_YjVlYzliMTAtY2IxOC00MDk1LTk0NmQtNDg1Mzg2YzIyMzM2%40thread.v2/0?context=%7b%22Tid%22%3a%22502eeaa5-f0f3-4c16-ad5b-e64d2e1f93c3%22%2c%22Oid%22%3a%22e63461e2-0fdc-488c-ad5a-b7ebb3b81d2d%22%7d',
   },
 ];
