bee4fd78f777fba06fcf97ed13182ab13d35b9e6
Tobias Görtz <tobias.goertz@zweitag.de>
2020-06-15 21:18:53 +0200

    add testaccount form

--- components/Form.vue
+++ components/Form.vue
@@ -9,7 +9,7 @@
     <div class="ms-form">
       <ClientOnly>
         <iframe
-          :data-src="formSrc"
+          :data-src="formSrc1"
           width="100%"
           height="2350px"
           frameborder="0"
@@ -24,7 +24,31 @@
         />
       </ClientOnly>
       <a
-        href="https://forms.microsoft.com/Pages/ResponsePage.aspx?id=peouUPPwFkytW-ZNLh-Tw-JhNObcD4xIrVq367O4HS1UQUU2VDI5T1pPVkVUS1FJMkNFU09URkJBQy4u&amp%3Bembed=true"
+        :href="formSrc1"
+        class="button primary"
+      >
+        Kontaktformular
+      </a>
+    </div>
+    <div class="ms-form">
+      <ClientOnly>
+        <iframe
+          :data-src="formSrc2"
+          width="100%"
+          height="1850px"
+          frameborder="0"
+          marginwidth="0"
+          marginheight="0"
+          style="border: none; max-width:100%; max-height:100%"
+          allowfullscreen=""
+          webkitallowfullscreen=""
+          mozallowfullscreen=""
+          msallowfullscreen=""
+          class="lazyload"
+        />
+      </ClientOnly>
+      <a
+        :href="formSrc2"
         class="button primary"
       >
         Kontaktformular
@@ -37,7 +61,8 @@
 export default {
   data() {
     return {
-      formSrc: 'https://forms.microsoft.com/Pages/ResponsePage.aspx?id=peouUPPwFkytW-ZNLh-Tw-JhNObcD4xIrVq367O4HS1UQUU2VDI5T1pPVkVUS1FJMkNFU09URkJBQy4u&amp;embed=true',
+      formSrc1: 'https://forms.microsoft.com/Pages/ResponsePage.aspx?id=peouUPPwFkytW-ZNLh-Tw-JhNObcD4xIrVq367O4HS1UQUU2VDI5T1pPVkVUS1FJMkNFU09URkJBQy4u&amp;embed=true',
+      formSrc2: 'https://forms.office.com/Pages/ResponsePage.aspx?id=peouUPPwFkytW-ZNLh-TwyoPZ9o80BhLvDqYhHG8NYtUM1VQT0ZXSFBPS1NXV0I3U0IxRDE2UFNCQyQlQCN0PWcu&amp%3Bembed=true',
     };
   },
 };
