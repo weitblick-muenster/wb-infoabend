51ef29b6d5b07d3ba5ce11dd660475cdfb82bbb6
Tobias Görtz <tobias.goertz@zweitag.de>
2020-10-15 21:36:12 +0200

    remove testaccount form

--- components/Form.vue
+++ components/Form.vue
@@ -11,7 +11,7 @@
         <iframe
           :data-src="formSrc1"
           width="100%"
-          height="2350px"
+          height="2550px"
           frameborder="0"
           marginwidth="0"
           marginheight="0"
@@ -30,30 +30,6 @@
         Kontaktformular
       </a>
     </div>
-    <div class="ms-form">
-      <ClientOnly>
-        <iframe
-          :data-src="formSrc2"
-          width="100%"
-          height="1850px"
-          frameborder="0"
-          marginwidth="0"
-          marginheight="0"
-          style="border: none; max-width:100%; max-height:100%"
-          allowfullscreen=""
-          webkitallowfullscreen=""
-          mozallowfullscreen=""
-          msallowfullscreen=""
-          class="lazyload"
-        />
-      </ClientOnly>
-      <a
-        :href="formSrc2"
-        class="button primary"
-      >
-        Kontaktformular
-      </a>
-    </div>
   </section>
 </template>
 
@@ -62,7 +38,6 @@ export default {
   data() {
     return {
       formSrc1: 'https://forms.microsoft.com/Pages/ResponsePage.aspx?id=peouUPPwFkytW-ZNLh-Tw-JhNObcD4xIrVq367O4HS1UQUU2VDI5T1pPVkVUS1FJMkNFU09URkJBQy4u&amp;embed=true',
-      formSrc2: 'https://forms.office.com/Pages/ResponsePage.aspx?id=peouUPPwFkytW-ZNLh-TwyoPZ9o80BhLvDqYhHG8NYtUM1VQT0ZXSFBPS1NXV0I3U0IxRDE2UFNCQyQlQCN0PWcu&amp%3Bembed=true',
     };
   },
 };
