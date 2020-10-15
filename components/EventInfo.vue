6d86486f02338257de53f1d40d052ee867c688dc
Tobias Görtz <tobias.goertz@zweitag.de>
2020-10-15 21:31:57 +0200

    change eventinfo

--- components/EventInfo.vue
+++ components/EventInfo.vue
@@ -3,7 +3,8 @@
     <h1>So funktioniert es</h1>
     <ul class="explanation">
       <li class="explanation-list-element">
-        💻 Aufzeichung vom Infoabend anschauen
+        <!-- 💻 Aufzeichung vom Infoabend anschauen -->
+        💻 Online Infoabend mit Live-Stream und anschließendem Kennenlernen per Video-Call
       </li>
       <li class="explanation-list-element">
         💌 Hinterlasse der Gruppe, die du interessant findest,
@@ -18,15 +19,5 @@
         🎉 Unsere Gruppen melden sich bei dir und du kannst direkt loslegen
       </li>
     </ul>
-    <br>
-    <div class="yt-video lazyload">
-      <iframe
-        title="Weitblick Live Stream"
-        src="https://www.youtube.com/embed/kr91LP4ecDk"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope;"
-        allowfullscreen
-      />
-    </div>
   </section>
 </template>
