547f90f8620586844395412da896c7e453c5f123
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-02-15 14:55:51 +0100

    add @nuxtjs/html-validator

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,6 +54,7 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
+    '@nuxtjs/html-validator',
   ],
   /*
   ** Nuxt.js modules
