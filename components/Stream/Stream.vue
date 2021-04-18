f92e56bc6ad18265d80d5d6e0ec6be102825a5ea
Manh Tin Nguyen <30327069+ManhTin@users.noreply.github.com>
2021-04-18 13:35:35 +0200

    update stream url

--- components/Stream/Stream.vue
+++ components/Stream/Stream.vue
@@ -4,7 +4,7 @@
       <div class="yt-video lazyload stream-left">
         <iframe
           title="Weitblick Live Stream"
-          src="https://www.youtube.com/embed/ISXUrm_XIs4"
+          src="https://www.youtube.com/embed/J_88T_bILy0"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope;"
           allowfullscreen
